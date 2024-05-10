60403dd5e8416164c6cce370421274053a275310
ThanhB <bintri92@gmail.com>
2024-05-10 15:36:02 +0700

    change litte

--- sever.js
+++ sever.js
@@ -1,21 +1,14 @@
-var rect = require("./caculate");
-function solveRect(l, b) {
-  console.log("Solving for rectangle with l = " + l + " and b = " + b);
+const { createServer } = require('node:http');
 
-  if (l <= 0 || b <= 0) {
-    console.log(
-      "Rectangle dimensions should be greater than zero:  l = " +
-        l +
-        ",  and b = " +
-        b
-    );
-  } else {
-    console.log("The area of the rectangle is " + rect.area(l, b));
-    console.log("The perimeter of the rectangle is " + rect.perimeter(l, b));
-  }
-}
+const hostname = '127.0.0.1'; // localhost
+const port = 3000;
 
-solveRect(2, 4);
-solveRect(3, 5);
-solveRect(0, 5);
-solveRect(-3, 5);
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World Bui Tan Thanh');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
